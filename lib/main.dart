3e962fb5d148c86579df7631381c7d5824690e77
ziadhassan7 <ziadhassan7@gmail.com>
2023-09-05 01:47:57 +0300

    Initialize Notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flip_streak/app_constants/color_constants.dart';
 import 'package:flip_streak/app_constants/hive_keys.dart';
+import 'package:flip_streak/business/notification_util.dart';
 import 'package:flip_streak/data/shared_pref/hive_client.dart';
 import 'package:flip_streak/presentation/index/index_page.dart';
 import 'package:flip_streak/presentation/welcome/screen/welcome_screen.dart';
@@ -7,13 +8,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'business/app_wise/streak/streak_state_util.dart';
-import 'business/system_wise/system_util.dart';
+import 'business/system_util.dart';
 
 Future<void> main() async {
   //When you are initializing any future function, it is better to call,
   //ensure initialized as your first line of code, before runApp(),
   //and your initializers
+  /*
+    The WidgetFlutterBinding is used to interact with the Flutter engine.
+    init()s needs to call native code to initialize 'That thing',
+    and since the plugin needs to use platform channels to call the native code,
+    which is done asynchronously therefore you have to call ensureInitialized()
+    to make sure that you have an instance of the WidgetsBinding.
+   */
   WidgetsFlutterBinding.ensureInitialized();
+
+  //Initialize Notification
+  NotificationUtil.init();
 
   //Initialize Hive
   await Hive.initFlutter();
