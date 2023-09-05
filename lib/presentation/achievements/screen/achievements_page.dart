145b5b5eada894a9fd26a693d43749630dc624d8
ziadhassan7 <ziadhassan7@gmail.com>
2023-09-05 04:39:16 +0300

    Removed unused space

--- lib/presentation/achievements/screen/achievements_page.dart
+++ lib/presentation/achievements/screen/achievements_page.dart
@@ -1,5 +1,5 @@
-import 'package:flip_streak/presentation/achievements/widget/today_goal_widget.dart';
-import 'package:flip_streak/presentation/achievements/widget/top_widget.dart';
+import 'package:flip_streak/presentation/achievements/view/today_goal_widget.dart';
+import 'package:flip_streak/presentation/achievements/view/top_widget.dart';
 import 'package:flutter/material.dart';
 import '../../../app_constants/color_constants.dart';
 
