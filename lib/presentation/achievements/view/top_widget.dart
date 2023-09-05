e1c3b0ca6078b1fda7df78d42b4d5ab69f8566e1
ziadhassan7 <ziadhassan7@gmail.com>
2023-09-05 04:37:23 +0300

    Move to new Folder

---
+++ lib/presentation/achievements/view/top_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import '../../../../app_constants/color_constants.dart';
+import '../../views/heading_image.dart';
+import '../../views/text_inria_sans.dart';
+
+class AchievementTopWidget extends StatelessWidget {
+  const AchievementTopWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 54, left: 26),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextInriaSans("Your", size: 28, color: colorAccent,),
+              TextInriaSans("Finished Books", weight: FontWeight.bold, size: 30, color: colorAccent,),
+            ],
+          ),),
+
+
+        const Align(
+            alignment: AlignmentDirectional.topEnd,
+            child: HeadingImage("assets/headings/achievement_heading.svg", haveSpaceOnTop: true,)),
+      ],
+    );
+  }
+}
