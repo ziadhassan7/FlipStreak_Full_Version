cb6847fe3e819743f754195041c937e8e5cffeff
ziadhassan7 <ziadhassan7@gmail.com>
2023-09-05 04:39:48 +0300

    added new attributes (currentProgress, goal)

--- lib/presentation/achievements/view/circular_goal_widget.dart
+++ lib/presentation/achievements/view/circular_goal_widget.dart
@@ -4,7 +4,11 @@ import 'package:flip_streak/presentation/views/text_inria_sans.dart';
 import 'package:flutter/material.dart';
 
 class CircularGoalWidget extends StatelessWidget {
-  const CircularGoalWidget({Key? key}) : super(key: key);
+  const CircularGoalWidget({Key? key,
+    required this.currentProgress, required this.goal}) : super(key: key);
+
+  final int currentProgress;
+  final int goal;
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +23,7 @@ class CircularGoalWidget extends StatelessWidget {
         radius: 100,
       ),
 
-      child: Center(child: TextInriaSans("4/5", color: colorAccent, weight: FontWeight.bold, size: 18,)),
+      child: Center(child: TextInriaSans("$currentProgress/$goal", color: colorAccent, weight: FontWeight.bold, size: 18,)),
     );
   }
 }
