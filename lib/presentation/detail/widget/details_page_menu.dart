b9aaa4560f8194df1ee209c726fec5522c986685
ziadhassan7 <ziadhassan7@gmail.com>
2023-09-15 01:44:54 +0300

    add note from menu in detail page

--- lib/presentation/detail/widget/details_page_menu.dart
+++ lib/presentation/detail/widget/details_page_menu.dart
@@ -1,3 +1,4 @@
+import 'package:flip_streak/presentation/views/dialoq/note_dialog/add_note_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../app_constants/color_constants.dart';
@@ -19,22 +20,32 @@ class DetailsPageMenu extends StatelessWidget {
               menuItems: [
                 PopupMenuItem<int>(
                   value: 0,
-                  child: TextInriaSans("Edit Last Page", color: colorAccent,),
+                  child: TextInriaSans("Add note", color: colorAccent,),
                 ),
 
                 PopupMenuItem<int>(
                   value: 1,
+                  child: TextInriaSans("Edit Last Page", color: colorAccent,),
+                ),
+
+                PopupMenuItem<int>(
+                  value: 2,
                   child: TextInriaSans("Mark As Complete", color: colorAccent,),
                 ),
               ],
 
               functions: (value) {
                 if(value == 0){
+                  //open Dialog to add note
+                  AddNoteDialog(context, ref,);
+                }
+
+                if(value == 1){
                   //open Dialog showing list of bookmarks
                   EditLastPageDialog(context, ref);
                 }
 
-                if (value == 1) {
+                if (value == 2) {
                   ref.read(bookListProvider.notifier).toggleAsCompleted(bookModel);
                 }
               }
